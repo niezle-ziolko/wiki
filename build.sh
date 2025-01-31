41b3aef034ac6ef6d09ef6a80df6012873d7c7d4
Norman Sobczyk <wgwcompany@duck.com>
2025-01-31 11:31:43 +0000

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 echo "Building the project..."
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
+python3.9 pip install django-cf
 python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
