31b118d94572d4913152f9a897234d8e9b5450d6
Norman Sobczyk <wgwcompany@duck.com>
2025-01-31 11:19:09 +0000

    update

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -8,23 +8,23 @@ from django.core.files.storage import default_storage
 # Import default_storage to interact with the default file storage system in Django
 
 def list_entries():
-    """
+    '''
     Returns a list of all names of encyclopedia entries.
-    """
-    _, filenames = default_storage.listdir("entries")
+    '''
+    _, filenames = default_storage.listdir('entries')
     # Get the list of filenames in the 'entries' directory from default storage
 
-    return list(sorted(re.sub(r"\.md$", "", filename)
-                for filename in filenames if filename.endswith(".md")))
+    return list(sorted(re.sub(r'\.md$', '', filename)
+                for filename in filenames if filename.endswith('.md')))
     # Remove the '.md' extension from filenames and return the sorted list of entry names
 
 def save_entry(title, content):
-    """
+    '''
     Saves an encyclopedia entry, given its title and Markdown
     content. If an existing entry with the same title already exists,
     it is replaced.
-    """
-    filename = f"entries/{title}.md"
+    '''
+    filename = f'entries/{title}.md'
     # Create the file path for the entry based on its title
 
     if default_storage.exists(filename):
@@ -35,15 +35,15 @@ def save_entry(title, content):
     # Save the new content for the entry
 
 def get_entry(title):
-    """
+    '''
     Retrieves an encyclopedia entry by its title. If no such
     entry exists, the function returns None.
-    """
+    '''
     try:
-        f = default_storage.open(f"entries/{title}.md")
+        f = default_storage.open(f'entries/{title}.md')
         # Try to open the file for the given entry title
 
-        return f.read().decode("utf-8")
+        return f.read().decode('utf-8')
         # Return the content of the file as a UTF-8 decoded string
     except FileNotFoundError:
         return None
--- wiki/urls.py
+++ wiki/urls.py
@@ -1,4 +1,4 @@
-"""wiki URL Configuration
+'''wiki URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -12,11 +12,11 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("encyclopedia.urls"))
+    path('', include('encyclopedia.urls'))
 ]
